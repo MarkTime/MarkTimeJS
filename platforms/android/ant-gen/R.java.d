3a7c1771a648e45f3b872c14aec68b59256b25e6
boar401s2 <johnsBogAcc@gmail.com>
2014-04-03 16:00:35 +1000

    Imported into eclipse

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -6,4 +6,9 @@ C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-mdpi\
 C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-xhdpi\icon.png \
 C:\Development\android_workspace\MarkTimeJS\platforms\android\res\values\strings.xml \
 C:\Development\android_workspace\MarkTimeJS\platforms\android\res\xml\config.xml \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable\icon.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-hdpi\icon.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-ldpi\icon.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-mdpi\icon.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
 C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\AndroidManifest.xml \
