0e5dac9e9744caf1460405336cabd98689c1fbb6
boar401s2 <johnsBogAcc@gmail.com>
2014-04-03 16:50:10 +1000

    Updated the icon

--- platforms/android/ant-build/MarkTime.ap_.d
+++ platforms/android/ant-build/MarkTime.ap_.d
@@ -1,16 +1,14 @@
 C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\MarkTime.ap_ \
- : C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-hdpi\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-ldpi\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-mdpi\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-xhdpi\icon.png \
+ : C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-hdpi\ic_launcher.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-mdpi\ic_launcher.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-xxhdpi\ic_launcher.png \
 C:\Development\android_workspace\MarkTimeJS\platforms\android\res\values\strings.xml \
 C:\Development\android_workspace\MarkTimeJS\platforms\android\res\xml\config.xml \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-hdpi\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-ldpi\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-mdpi\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-hdpi\ic_launcher.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-mdpi\ic_launcher.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-xhdpi\ic_launcher.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-xxhdpi\ic_launcher.png \
 C:\Development\android_workspace\MarkTimeJS\platforms\android\assets\www \
 C:\Development\android_workspace\MarkTimeJS\platforms\android\assets\_where-is-www.txt \
 C:\Development\android_workspace\MarkTimeJS\platforms\android\assets\www\cordova.js \
--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -1,14 +1,12 @@
 C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-gen\marktimejs\boar401s2\R.java \
- : C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-hdpi\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-ldpi\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-mdpi\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-xhdpi\icon.png \
+ : C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-hdpi\ic_launcher.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-mdpi\ic_launcher.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\res\drawable-xxhdpi\ic_launcher.png \
 C:\Development\android_workspace\MarkTimeJS\platforms\android\res\values\strings.xml \
 C:\Development\android_workspace\MarkTimeJS\platforms\android\res\xml\config.xml \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-hdpi\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-ldpi\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-mdpi\icon.png \
-C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-hdpi\ic_launcher.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-mdpi\ic_launcher.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-xhdpi\ic_launcher.png \
+C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\res\drawable-xxhdpi\ic_launcher.png \
 C:\Development\android_workspace\MarkTimeJS\platforms\android\ant-build\AndroidManifest.xml \
